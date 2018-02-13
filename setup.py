ae6d26ae580feae97ac18e938fbc3c10e5b6575a
actionless <actionless.loveless@gmail.com>
2018-02-13 20:57:19 +0100

    fix(setup.py): correct license

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GPL3 License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.

4924f8fc6fd65fd3a311dc553e2e7312ae25770b
actionless <actionless.loveless@gmail.com>
2018-02-13 20:55:50 +0100

    chore(maintenance_scripts: tag_release): update setup.py version as well

--- maintenance_scripts/tag_release.sh
+++ maintenance_scripts/tag_release.sh
@@ -4,8 +4,10 @@ IFS=$'\n\t'
 
 new_version=$1
 
-if [[ $(git status --porcelain 2>/dev/null| grep "^ [MD]" | wc -l) -gt 0 ]] ; then
-	echo "You have uncommited changes"
+if [[ $(git status --porcelain 2>/dev/null| grep -c "^ [MD]") -gt 0 ]] ; then
+	echo
+	echo "   !!! You have uncommited changes: !!!"
+	echo
 	git status
 	exit 1
 fi
@@ -13,5 +15,6 @@ fi
 sed -i -e "s/pkgver=.*/pkgver=${new_version}/g" PKGBUILD
 sed -i -e "s/pkgrel=.*/pkgrel=1/g" PKGBUILD
 sed -i -e "s/VERSION.*=.*/VERSION = '${new_version}'/g" pikaur/config.py
+sed -i -e "s/    version='.*',/    version='${new_version}',/g" setup.py
 git commit -am "chore: bump version to ${new_version}"
 git tag -a "${new_version}"

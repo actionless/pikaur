e4cf9087eee255ce605261b56cb37588e2638d9e
actionless <actionless.loveless@gmail.com>
2018-02-18 04:15:36 +0100

    feat(install cli): rename build dir after successfull transaction

--- pikaur/install_cli.py
+++ pikaur/install_cli.py
@@ -120,6 +120,7 @@ class InstallPackagesCLI():
             for package_build in self.package_builds.values():
                 if package_build.built_package_path:
                     package_build.update_last_installed_file()
+                    remove_dir(package_build.build_dir)
 
         if self.failed_to_build:
             print('\n'.join(

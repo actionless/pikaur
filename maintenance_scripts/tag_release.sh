22272489a2b4ba324787beff84782db569b5f4f6
actionless <actionless.loveless@gmail.com>
2018-02-22 03:05:11 +0100

    chor(maintenance_scripts: tag_release): force tag

--- maintenance_scripts/tag_release.sh
+++ maintenance_scripts/tag_release.sh
@@ -16,5 +16,5 @@ sed -i -e "s/pkgver=.*/pkgver=${new_version}/g" PKGBUILD
 sed -i -e "s/pkgrel=.*/pkgrel=1/g" PKGBUILD
 sed -i -e "s/VERSION.*=.*/VERSION = '${new_version}-dev'/g" pikaur/config.py
 sed -i -e "s/    version='.*',/    version='${new_version}',/g" setup.py
-git commit -am "chore: bump version to ${new_version}"
-git tag -a "${new_version}"
+git commit -am "chore: bump version to ${new_version}" || true
+git tag -a "${new_version}" -f

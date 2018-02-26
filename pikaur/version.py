a56487d6ffe0bc28c572412219d40dfed47f95c9
actionless <actionless.loveless@gmail.com>
2018-02-26 01:43:08 +0100

    chore(version): remove old version comparison implementation

--- pikaur/version.py
+++ pikaur/version.py
@@ -1,38 +1,4 @@
-from distutils.version import LooseVersion
 import ctypes
-
-
-def compare_versions_bak(current_version, new_version):
-    # @TODO: remove this before the release
-    if current_version != new_version:
-        for separator in ('+',):
-            current_version = current_version.replace(separator, '.')
-            new_version = new_version.replace(separator, '.')
-        current_base_version = new_base_version = None
-        for separator in (':',):
-            if separator in current_version:
-                current_base_version, _current_version = \
-                    current_version.split(separator)[:2]
-            if separator in new_version:
-                new_base_version, _new_version = \
-                    new_version.split(separator)[:2]
-            if (
-                    current_base_version and new_base_version
-            ) and (
-                current_base_version != new_base_version
-            ):
-                current_version = current_base_version
-                new_version = new_base_version
-                break
-
-        versions = [current_version, new_version]
-        try:
-            versions.sort(key=LooseVersion)
-        except TypeError:
-            # print(versions)
-            return False
-        return versions[1] == new_version
-    return False
 
 
 def compare_versions(current_version, new_version):
@@ -53,8 +19,6 @@ def compare_versions(current_version, new_version):
 
 
 def compare_versions_test():
-    import traceback
-
     for expected_result, old_version, new_version in (
             (-1, '0.2+9+123abc-1', '0.3-1'),
             (-1, '0.50.12', '0.50.13'),
@@ -63,14 +27,7 @@ def compare_versions_test():
             (-1, '0.50.2+1', '0.50.2+6+123131-1'),
             (0, '0.50.1', '0.50.1'),
     ):
-        print((old_version, new_version))
-        try:
-            assert compare_versions_bak(old_version, new_version) == (expected_result < 0)
-        except AssertionError:
-            traceback.print_exc()
         assert compare_versions(old_version, new_version) == expected_result
-
-    print("Tests passed!")
 
 
 class VersionMatcher():

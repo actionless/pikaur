2c8c15ec50b4db5a8a6df775dc62c964970b5f9d
actionless <actionless.loveless@gmail.com>
2018-03-02 08:00:24 +0100

    feat(pprint: progress bar): make it more pacman-styled

--- pikaur/pprint.py
+++ pikaur/pprint.py
@@ -313,17 +313,24 @@ class ProgressBar(object):
     index = 0
     progress = 0
 
+    LEFT_DECORATION = '['
+    RIGHT_DECORATION = ']'
+    EMPTY = '-'
+    FULL = '#'
+
     def __init__(self, length, message=''):
         self.message = message
-        width = get_term_width() - len(message)
+        width = get_term_width() - len(message) - len(self.LEFT_DECORATION) - len(self.RIGHT_DECORATION)
         self.print_ratio = length / width
         sys.stderr.write(message)
+        sys.stderr.write(self.LEFT_DECORATION + self.EMPTY * width + self.RIGHT_DECORATION)
+        sys.stderr.write(f'{(chr(27))}[\bb' * (width + len(self.RIGHT_DECORATION)))
 
     def update(self):
         self.index += 1
         if self.index / self.print_ratio > self.progress:
             self.progress += 1
-            sys.stderr.write('.')
+            sys.stderr.write(self.FULL)
             sys.stderr.flush()
 
     def __enter__(self):

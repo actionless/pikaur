2df5de59d8ed289f99cb118025a85753af593313
actionless <actionless.loveless@gmail.com>
2018-03-09 02:49:37 +0100

    refactor(i18n): typing

--- pikaur/i18n.py
+++ pikaur/i18n.py
@@ -1,11 +1,12 @@
 import gettext
 
+
 TRANSLATION = gettext.translation('pikaur', fallback=True)
 
 
-def _(msg):
+def _(msg: str) -> str:
     return TRANSLATION.gettext(msg)
 
 
-def _n(singular, plural, count):
+def _n(singular: str, plural: str, count: int) -> str:
     return TRANSLATION.ngettext(singular, plural, count)

31fe0a4b04949798c17415f1bfc10f4e5dfa1d4b
actionless <actionless.loveless@gmail.com>
2018-03-23 20:40:42 +0100

    refactor(core: configreader): use own implementation instead of extending default `configparser` module
    re #18

--- pikaur/core.py
+++ pikaur/core.py
@@ -1,10 +1,9 @@
-import configparser
 import os
 import shutil
 import subprocess
 import enum
 import codecs
-from typing import Dict, Any, List, Iterable
+from typing import Dict, Any, List, Iterable, Tuple
 
 
 NOT_FOUND_ATOM = object()
@@ -58,43 +57,47 @@ def open_file(file_path: str, mode='r', encoding='utf-8'):
 
 class ConfigReader():
 
-    _cached_config: Dict[str, configparser.SectionProxy] = None
+    _cached_config: Dict[str, Dict[str, str]] = None
     default_config_path: str = None
     list_fields: List[str] = []
     ignored_fields: List[str] = []
 
-    @classmethod
-    def _approve_line_for_parsing(cls, line: str) -> bool:
-        if line.startswith(' '):
-            return False
-        if '=' not in line:
-            return False
-        if not cls.ignored_fields:
-            return True
-        field_area = line.split('=')[0]
-        for field in cls.ignored_fields:
-            if field in field_area:
-                return False
-        return True
+    comment_prefixes = ('#', ';')
 
     @classmethod
-    def get_config(cls, config_path: str = None) -> configparser.SectionProxy:
+    def _parse_line(cls, line: str) -> Tuple[str, str]:
+        blank = (None, None, )
+        if line.startswith(' '):
+            return blank
+        if '=' not in line:
+            return blank
+        line = line.strip()
+        for comment_prefix in cls.comment_prefixes:
+            line, *_comments = line.split(comment_prefix)
+
+        key, *values = line.split('=')
+        value = '='.join(values)
+        key = key.strip()
+        value = value.strip()
+
+        if key in cls.ignored_fields:
+            return blank
+        return key, value
+
+    @classmethod
+    def get_config(cls, config_path: str = None) -> Dict[str, str]:
         config_path = config_path or cls.default_config_path
         if not cls._cached_config:
             cls._cached_config = {}
         if not cls._cached_config.get(config_path):
-            config = configparser.ConfigParser(
-                allow_no_value=True,
-                strict=False,
-                inline_comment_prefixes=('#', ';')
-            )
             with open_file(config_path) as config_file:
-                config_string = '\n'.join(['[all]'] + [
-                    line for line in config_file.readlines()
-                    if cls._approve_line_for_parsing(line)
-                ])
-            config.read_string(config_string)
-            cls._cached_config[config_path] = config['all']
+                cls._cached_config[config_path] = {
+                    key: value
+                    for key, value in [
+                        cls._parse_line(line)
+                        for line in config_file.readlines()
+                    ] if key
+                }
         return cls._cached_config[config_path]
 
     @classmethod

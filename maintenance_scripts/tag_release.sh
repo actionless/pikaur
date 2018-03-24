74ba5c91614c81fa54b0f1049399404cd1abdb43
actionless <actionless.loveless@gmail.com>
2018-03-25 00:52:00 +0100

    chore(tag_release): create stable PKGBUILD
    re #78

--- maintenance_scripts/tag_release.sh
+++ maintenance_scripts/tag_release.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2016
 set -euo pipefail
 IFS=$'\n\t'
+aur_repo_dir=~/build/pikaur
+aur_dev_repo_dir=~/build/pikaur-git
 
 new_version=$1
 
@@ -18,3 +21,35 @@ sed -i -e "s/VERSION.*=.*/VERSION = '${new_version}-dev'/g" pikaur/config.py
 sed -i -e "s/    version='.*',/    version='${new_version}',/g" setup.py
 git commit -am "chore: bump version to ${new_version}" || true
 git tag -a "${new_version}" -f
+
+echo "[configm push to pikaur git repo?]"
+read -r
+git push origin HEAD
+
+
+cp PKGBUILD "${aur_dev_repo_dir}"/PKGBUILD
+# shellcheck disable=SC2164
+cd "${aur_dev_repo_dir}"
+makepkg --printsrcinfo > .SRCINFO
+git add PKGBUILD .SRCINFO
+git commit -m "update to ${new_version}"
+
+echo "[confirm push to AUR dev package?]"
+read -r
+git push origin HEAD
+
+
+sed \
+	-e 's|pkgname=pikaur-git|pkgname=pikaur|' \
+	-e 's|"$pkgname::git+https://github.com/actionless/pikaur.git#branch=master"|"$pkgname-$pkgver.tar.gz"::https://github.com/actionless/pikaur/archive/"$pkgver".tar.gz|' \
+	-e '/pkgver() {/,+5 d' \
+	PKGBUILD > "${aur_repo_dir}"/PKGBUILD
+# shellcheck disable=SC2164
+cd "${aur_repo_dir}"
+makepkg --printsrcinfo > .SRCINFO
+git add PKGBUILD .SRCINFO
+git commit -m "update to ${new_version}"
+
+echo "[confirm push to AUR stable package?]"
+read -r
+git push origin HEAD

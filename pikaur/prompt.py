ccf3ed34e702f0b09e47dac9355cc524830205d0
Jake Stanger <jakestanger@gmail.com>
2018-03-29 12:35:59 +0100

    Convert answers to lowercase when comparing.
    Fixes uppercase choices being ignored i.e in cases where comparing Y to y.

--- pikaur/prompt.py
+++ pikaur/prompt.py
@@ -41,7 +41,7 @@ def read_answer_from_tty(question: str, answers: str = 'Yn') -> str:
         if ord(answer) == 13:
             answer = default
             return default
-        if answer in answers:
+        if answer in [a.lower() for a in answers]:
             return answer
         return ' '
     except Exception:

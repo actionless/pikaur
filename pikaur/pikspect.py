281f65286db4abe5b3fdd949b1c58bd3b52f52e7
actionless <actionless.loveless@gmail.com>
2018-04-22 13:46:24 +0200

    feat(pikspect): implement `print_output` and `save_output` options

--- pikaur/pikspect.py
+++ pikaur/pikspect.py
@@ -2,7 +2,6 @@ import sys
 import subprocess
 import tty
 import pty
-import traceback
 import gettext
 import io
 import termios
@@ -10,11 +9,16 @@ import select
 import shutil
 import struct
 import fcntl
-from multiprocessing.pool import ThreadPool
-from typing import List, Tuple, Callable, Any
+import uuid
+import os
+import multiprocessing
 from time import sleep
+from typing import List, Tuple, Union
 
-from .pprint import bold_line, print_status_message
+from .core import DataType
+from .pprint import bold_line, PrintLock, print_stdout
+from .threading import handle_exception_in_thread, ThreadSafeBytesStorage
+
 
 PACMAN_TRANSLATION = gettext.translation('pacman', fallback=True)
 
@@ -33,60 +37,70 @@ DEFAULT_QUESTIONS = [
 DEFAULT_ANSWER = _p("Y")
 
 
-def handle_exception_in_thread(fun: Callable) -> Callable:
+SMALL_TIMEOUT = 0.1
 
-    def decorated(proc: subprocess.Popen, *args: Any, **kwargs: Any):
-        try:
-            return fun(proc, *args, **kwargs)
-        # except OSError:
-        # pass
-        except Exception as exc:
-            print_status_message('Error in the thread:')
-            traceback.print_exc()
-            raise exc
 
-    return decorated
+class PikspectPopen(subprocess.Popen):
+
+    saved_bytes: bytes = None
+
+
+class PikspectTaskData(DataType):
+    proc: PikspectPopen = None
+    pty_out: io.BytesIO = None
+    pty_in: io.TextIOWrapper = None
+    default_answer: str = None
+    default_questions: Tuple[str] = None
+    print_output: bool = None
+    save_output: bool = None
+    task_id: uuid.UUID = None
 
 
 @handle_exception_in_thread
-def output_handler(
-        proc: subprocess.Popen,
-        proc_output_reader: io.BytesIO,
-        pty_in: io.TextIOWrapper,
-        default_answer: str,
-        default_questions: Tuple[str],
-) -> None:
-    historic_output = b''
-    max_question_length = max([len(q) for q in default_questions]) + 10
+def cmd_output_handler(task_data: PikspectTaskData) -> None:
+    historic_output: List[bytes] = []
+    max_question_length = max([len(q) for q in task_data.default_questions]) + 10
+    default_questions_bytes = [
+        [chr(char).encode('utf-8') for char in question.encode('utf-8')]
+        for question in task_data.default_questions
+    ]
     while True:
-        if proc.returncode is not None:
-            break
-        output = proc_output_reader.read(1)
+        # if proc.returncode is not None:
+            # break
+        output = task_data.pty_out.read(1)
         if not output:
             break
-        historic_output = historic_output[-max_question_length:] + output
+        historic_output = historic_output[-max_question_length:] + [output, ]
 
-        sys.stdout.buffer.write(output)
-        sys.stdout.buffer.flush()
-        for question in default_questions:
-            if historic_output.endswith(question.encode('utf-8')):
-                sys.stdout.write(default_answer + '\n')
-                sys.stdout.flush()
-                pty_in.write(default_answer)
-                sleep(0.1)
-                pty_in.write('\n')
-                historic_output = b''
-                break
-    print_status_message('output handler exiting')
+        if task_data.print_output:
+            with PrintLock():
+                sys.stdout.buffer.write(output)
+                sys.stdout.buffer.flush()
+        if task_data.save_output:
+            ThreadSafeBytesStorage.add_bytes(task_data.task_id, output)
+        for question in default_questions_bytes:
+            if len(historic_output) < len(question) or (
+                    historic_output[-len(question):] != question
+            ):
+                continue
+            if task_data.print_output:
+                print_stdout(task_data.default_answer + '\n')
+            with PrintLock():
+                if task_data.save_output:
+                    ThreadSafeBytesStorage.add_bytes(
+                        task_data.task_id, task_data.default_answer.encode('utf-8') + b'\n'
+                    )
+                task_data.pty_in.write(task_data.default_answer)
+                sleep(SMALL_TIMEOUT)
+                task_data.pty_in.write('\n')
+            historic_output = []
+            break
 
 
 @handle_exception_in_thread
-def input_reader(
-        proc: subprocess.Popen,
-        pty_in: io.TextIOWrapper,
-) -> None:
+def user_input_reader(task_data: PikspectTaskData) -> None:
     while True:
-        if proc.returncode is not None:
+        if task_data.proc.returncode is not None:
             break
         char = None
 
@@ -98,26 +112,23 @@ def input_reader(
             else:
                 return
         else:
-            sleep(0.1)
+            sleep(SMALL_TIMEOUT)
             continue
 
         try:
-            pty_in.write(char)
+            with PrintLock():
+                task_data.pty_in.write(char)
         except ValueError:
             return
-        sys.stdout.write(char)
-        sys.stdout.flush()
+        if task_data.print_output:
+            print_stdout(char)
+        if task_data.save_output:
+            ThreadSafeBytesStorage.add_bytes(task_data.task_id, char.encode('utf-8'))
 
 
 @handle_exception_in_thread
-def communicator(proc: subprocess.Popen) -> None:
-    proc.communicate()
-    if sys.stdin.isatty():
-        termios.tcflush(sys.stdin.fileno(), termios.TCIOFLUSH)
-    if sys.stderr.isatty():
-        termios.tcdrain(sys.stderr.fileno())
-    if sys.stdout.isatty():
-        termios.tcdrain(sys.stdout.fileno())
+def communicator(task_data: PikspectTaskData) -> None:
+    task_data.proc.communicate()
 
 
 def set_terminal_geometry(file_descriptor: int, rows: int, columns: int) -> None:
@@ -125,60 +136,88 @@ def set_terminal_geometry(file_descriptor: int, rows: int, columns: int) -> None
     fcntl.ioctl(file_descriptor, termios.TIOCSWINSZ, term_geometry_struct)
 
 
+class NestedTerminal():
+
+    old_tcattrs: List[Union[int, List[bytes]]] = None
+
+    def __enter__(self) -> os.terminal_size:
+        real_term_geometry = shutil.get_terminal_size((80, 80))
+        if sys.stdin.isatty():
+            self.old_tcattrs = termios.tcgetattr(sys.stdin.fileno())
+            tty.setcbreak(sys.stdin.fileno())
+        if sys.stderr.isatty():
+            tty.setcbreak(sys.stderr.fileno())
+        if sys.stdout.isatty():
+            tty.setcbreak(sys.stdout.fileno())
+        return real_term_geometry
+
+    def __exit__(self, *exc_details) -> None:
+        if sys.stderr.isatty():
+            termios.tcdrain(sys.stderr.fileno())
+        if sys.stdout.isatty():
+            termios.tcdrain(sys.stdout.fileno())
+        if sys.stdin.isatty():
+            termios.tcflush(sys.stdin.fileno(), termios.TCIOFLUSH)
+            termios.tcsetattr(sys.stdin.fileno(), termios.TCSANOW, self.old_tcattrs)
+
+
+# pylint: disable=too-many-locals
 def pikspect(
         cmd: List[str],
         default_answer=DEFAULT_ANSWER,
         default_questions=DEFAULT_QUESTIONS,
+        print_output=True,
+        save_output=True,
         **kwargs
-) -> subprocess.Popen:
+) -> PikspectPopen:
 
-    real_term_geometry = shutil.get_terminal_size((80, 80))
-    if sys.stdin.isatty():
-        old_tcattrs = termios.tcgetattr(sys.stdin.fileno())
-        tty.setcbreak(sys.stdin.fileno())
-    if sys.stderr.isatty():
-        tty.setcbreak(sys.stderr.fileno())
-    if sys.stdout.isatty():
-        tty.setcbreak(sys.stdout.fileno())
+    task_id = uuid.uuid4()
 
-    pty_master, pty_slave = pty.openpty()
-    pty_master2, pty_slave2 = pty.openpty()
-    with ThreadPool() as pool:
-        with open(pty_master, 'w') as pty_in:
-            pty_out = open(pty_master2, 'rb')
-            set_terminal_geometry(
-                pty_out.fileno(),
-                columns=real_term_geometry.columns,
-                rows=real_term_geometry.lines
+    with NestedTerminal() as real_term_geometry:
+        pty_user_master, pty_user_slave = pty.openpty()
+        pty_cmd_master, pty_cmd_slave = pty.openpty()
+        pty_out = open(pty_cmd_master, 'rb')
+        set_terminal_geometry(
+            pty_out.fileno(),
+            columns=real_term_geometry.columns,
+            rows=real_term_geometry.lines
+        )
+
+        if 'sudo' in cmd:
+            subprocess.run(['sudo', '-v'])
+        proc = PikspectPopen(
+            cmd,
+            stdin=pty_user_slave,
+            stdout=pty_cmd_slave,
+            stderr=pty_cmd_slave,
+            **kwargs
+        )
+        with open(pty_user_master, 'w') as pty_in:
+            task_data = PikspectTaskData(
+                proc=proc,
+                pty_out=pty_out,
+                pty_in=pty_in,
+                default_answer=default_answer,
+                default_questions=default_questions,
+                print_output=print_output,
+                save_output=save_output,
+                task_id=task_id
             )
+            with multiprocessing.pool.ThreadPool() as pool:
+                output_task = pool.apply_async(cmd_output_handler, (task_data, ))
+                pool.apply_async(user_input_reader, (task_data, ))
+                communicate_task = pool.apply_async(communicator, (task_data, ))
 
-            if 'sudo' in cmd:
-                subprocess.run(['sudo', '-v'])
-            proc = subprocess.Popen(
-                cmd,
-                stdin=pty_slave,
-                stdout=pty_slave2,
-                # stderr=pty_slave2,
-                stderr=subprocess.STDOUT,
-                **kwargs)
-            pool.apply_async(output_handler, (
-                proc,
-                pty_out,
-                pty_in,
-                default_answer,
-                default_questions,
-            ))
-            pool.apply_async(input_reader, (
-                proc,
-                pty_in,
-            ))
-            communicate_task = pool.apply_async(communicator, (proc, ))
+                pool.close()
+                communicate_task.get()
+                try:
+                    output_task.get(timeout=SMALL_TIMEOUT)
+                except multiprocessing.context.TimeoutError:
+                    pass
+                pool.terminate()
 
-            pool.close()
-            communicate_task.get()
-            pool.terminate()
-    if sys.stdin.isatty():
-        termios.tcsetattr(sys.stdin.fileno(), termios.TCSANOW, old_tcattrs)
+    if save_output:
+        proc.saved_bytes = ThreadSafeBytesStorage.get_bytes_output(task_id)
     return proc
 
 
---
+++ pikaur/threading.py
@@ -0,0 +1,72 @@
+import traceback
+import uuid
+from threading import Lock
+from typing import List, Callable, Any, Dict
+
+from .pprint import print_status_message
+
+
+class ThreadSafeSequentialStorage():
+
+    _storage: Dict[uuid.UUID, Any] = {}
+    _locks: Dict[uuid.UUID, Lock] = {}
+
+    @classmethod
+    def _check_lock_and_storage(cls, _id) -> None:
+        if _id not in cls._storage:
+            cls._storage[_id] = []
+            cls._locks[_id] = Lock()
+
+    @classmethod
+    def _get_storage(cls, _id: uuid.UUID) -> List:
+        cls._check_lock_and_storage(_id)
+        return cls._storage[_id]
+
+    @classmethod
+    def _get_lock(cls, _id: uuid.UUID) -> Lock:
+        cls._check_lock_and_storage(_id)
+        return cls._locks[_id]
+
+    @classmethod
+    def _add_item(cls, _id: uuid.UUID, item: Any) -> None:
+        lock = cls._get_lock(_id)
+        lock.acquire()
+        cls._get_storage(_id).append(item)
+        lock.release()
+
+
+class ThreadSafeLineStorage(ThreadSafeSequentialStorage):
+
+    @classmethod
+    def add_line(cls, _id: uuid.UUID, line: str) -> None:
+        cls._add_item(_id, line)
+
+    @classmethod
+    def get_output(cls, _id: uuid.UUID) -> str:
+        return '\n'.join(cls._get_storage(_id))
+
+
+class ThreadSafeBytesStorage(ThreadSafeSequentialStorage):
+
+    @classmethod
+    def add_bytes(cls, _id: uuid.UUID, chars: bytes) -> None:
+        super()._add_item(_id, chars)
+
+    @classmethod
+    def get_bytes_output(cls, _id: uuid.UUID) -> bytes:
+        return b''.join(cls._get_storage(_id))
+
+
+def handle_exception_in_thread(fun: Callable) -> Callable:
+
+    def decorated(*args: Any, **kwargs: Any):
+        try:
+            return fun(*args, **kwargs)
+        # except OSError:
+        # pass
+        except Exception as exc:
+            print_status_message('Error in the thread:')
+            traceback.print_exc()
+            raise exc
+
+    return decorated

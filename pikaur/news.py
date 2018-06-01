4422f2c8a4ef7395af01323aa9c95c51003affcf
steven-omaha <steven-omaha@github>
2018-06-01 22:40:15 +0200

    fixed code checks

--- pikaur/news.py
+++ pikaur/news.py
@@ -1,7 +1,9 @@
 import datetime
 import urllib.request
 import urllib.error
+import urllib.response
 import xml.etree.ElementTree
+from xml.etree.ElementTree import ElementTree
 import os
 from http.client import HTTPResponse
 from html.parser import HTMLParser
@@ -10,7 +12,9 @@ from typing import TextIO, Union
 
 from pikaur.config import CACHE_ROOT
 from pikaur.pprint import color_line, format_paragraph, print_stdout
-#from pikaur.pacman import
+
+
+# from pikaur.pacman import
 
 
 # TODO internationalization
@@ -26,6 +30,8 @@ class News(object):
         self._news_feed = self._get_rss_feed()
 
     def print_latest(self) -> None:
+        if not isinstance(self._news_feed, ElementTree):
+            return
         if not self._is_new(self._last_online_news(self._news_feed)):
             return
         news_entry: xml.etree.ElementTree.Element
@@ -33,25 +39,25 @@ class News(object):
             child: xml.etree.ElementTree.Element
             for child in news_entry:
                 if 'pubDate' in child.tag:
-                    if self._is_new(child.text):
+                    if self._is_new(str(child.text)):
                         self._print_one_entry(news_entry)
                     else:
                         # no more news
                         return
 
-    def _get_rss_feed(self) -> Union[xml.etree.ElementTree.ElementTree, None]:
+    def _get_rss_feed(self) -> Union[xml.etree.ElementTree.Element, None]:
         try:
-            http_response: HTTPResponse = urllib.request.urlopen(
+            http_response: Union[HTTPResponse, urllib.response.addinfourl] = urllib.request.urlopen(
                 self.URL + self.DIR
             )
         except urllib.error.URLError:
             print_stdout('Could not fetch archlinux.org news')
-            return
+            return None
         str_response: str = ''
         for line in http_response:
             str_response += line.decode('UTF-8').strip()
         if not str_response:  # could not get data
-            return
+            return None
         return xml.etree.ElementTree.fromstring(str_response)
 
     @staticmethod
@@ -63,11 +69,10 @@ class News(object):
             child: xml.etree.ElementTree.Element
             for child in news_entry:
                 if 'pubDate' in child.tag:
-                    return child.text
+                    return str(child.text)
         # if we get to here, then something went really wrong
         # no valid news found
         return ''
-
 
     @staticmethod
     def _get_last_seen_news() -> str:
@@ -95,7 +100,7 @@ class News(object):
         )
         if not last_online_news:
             raise ValueError('The news feed could not be received or parsed.')
-        last_online_news_date: datetime = datetime.datetime.strptime(
+        last_online_news_date: datetime.datetime = datetime.datetime.strptime(
             last_online_news, '%a, %d %b %Y %H:%M:%S %z'
         )
         return last_online_news_date > last_seen_news_date
@@ -106,11 +111,11 @@ class News(object):
         child: xml.etree.ElementTree.Element
         for child in news_entry:
             if 'title' in child.tag:
-                title: str = child.text
+                title = str(child.text)
             if 'pubDate' in child.tag:
-                pub_date: str = child.text
+                pub_date = str(child.text)
             if 'description' in child.tag:
-                description: str = child.text
+                description = str(child.text)
         print_stdout(
             color_line(title, 11) + ' (' + pub_date + ')'
         )
@@ -129,7 +134,7 @@ class MLStripper(HTMLParser):
         self.reset()
         self.strict = False
         self.convert_charrefs = True
-        self.fed = []
+        self.fed: list = []
 
     def handle_data(self, data: object) -> None:
         self.fed.append(data)
@@ -138,7 +143,7 @@ class MLStripper(HTMLParser):
         return ''.join(self.fed)
 
 
-def strip_tags(html: object) -> str:
+def strip_tags(html: str) -> str:
     mlstripper = MLStripper()
     mlstripper.feed(html)
     return mlstripper.get_data()

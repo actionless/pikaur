69c8352dda7de1bebc92da920c61b48ef39304e1
actionless <actionless.loveless@gmail.com>
2018-06-08 05:42:41 +0200

    feat(pprint): purge line

--- pikaur/pprint.py
+++ pikaur/pprint.py
@@ -51,6 +51,10 @@ def get_term_width() -> int:
     return shutil.get_terminal_size((80, 80)).columns
 
 
+def purge_line(self):
+    print_stdout('\r' + ' ' * get_term_width() + '\033[1A')
+
+
 def format_paragraph(line: str) -> str:
     if not color_enabled():
         return PADDING * ' ' + line

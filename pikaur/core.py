4962e7237294b8f1c4d6bbb223c4b6f29106214d
actionless <actionless.loveless@gmail.com>
2018-06-10 22:34:33 +0200

    refactor(core, makepkg_config): move configreader from core

--- pikaur/core.py
+++ pikaur/core.py
@@ -5,12 +5,30 @@ import enum
 import codecs
 import distutils
 from distutils.dir_util import copy_tree
-from typing import (
-    Dict, Any, List, Iterable, Tuple, Union, Callable, Optional,
-)
+from typing import Any, List, Iterable, Callable
 
 
 NOT_FOUND_ATOM = object()
+
+
+class DataType():
+
+    def __init__(self, **kwargs) -> None:
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __setattr__(self, key: str, value: Any) -> None:
+        if (
+                not getattr(self, "__annotations__", None) or
+                self.__annotations__.get(key, NOT_FOUND_ATOM) is NOT_FOUND_ATOM  # pylint: disable=no-member
+        ) and (
+            getattr(self, key, NOT_FOUND_ATOM) is NOT_FOUND_ATOM
+        ):
+            raise TypeError(
+                f"'{self.__class__.__name__}' does "
+                f"not have attribute '{key}'"
+            )
+        super().__setattr__(key, value)
 
 
 class PackageSource(enum.Enum):
@@ -64,26 +82,6 @@ def sudo(cmd: List[str]) -> List[str]:
     return ['sudo', ] + cmd
 
 
-class DataType():
-
-    def __init__(self, **kwargs) -> None:
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def __setattr__(self, key: str, value: Any) -> None:
-        if (
-                not getattr(self, "__annotations__", None) or
-                self.__annotations__.get(key, NOT_FOUND_ATOM) is NOT_FOUND_ATOM  # pylint: disable=no-member
-        ) and (
-            getattr(self, key, NOT_FOUND_ATOM) is NOT_FOUND_ATOM
-        ):
-            raise TypeError(
-                f"'{self.__class__.__name__}' does "
-                f"not have attribute '{key}'"
-            )
-        super().__setattr__(key, value)
-
-
 def detect_bom_type(file_path: str) -> str:
     """
     returns file encoding string for open() function
@@ -128,70 +126,6 @@ def open_file(
     return codecs.open(
         file_path, mode, errors='ignore', **kwargs
     )
-
-
-CONFIG_VALUE_TYPE = Union[None, str, List[str]]
-CONFIG_FORMAT = Dict[str, CONFIG_VALUE_TYPE]
-
-
-class ConfigReader():
-
-    comment_prefixes = ('#', ';')
-
-    _cached_config: Optional[Dict[str, CONFIG_FORMAT]] = None
-    default_config_path: str
-    list_fields: List[str] = []
-    ignored_fields: List[str] = []
-
-    @classmethod
-    def _parse_line(cls, line: str) -> Tuple[Optional[str], CONFIG_VALUE_TYPE]:
-        blank = (None, None, )
-        if line.startswith(' '):
-            return blank
-        if '=' not in line:
-            return blank
-        line = line.strip()
-        for comment_prefix in cls.comment_prefixes:
-            line, *_comments = line.split(comment_prefix)
-
-        key, *values = line.split('=')
-        value = '='.join(values)
-        key = key.strip()
-        value = value.strip()
-
-        if key in cls.ignored_fields:
-            return blank
-
-        if value:
-            value = value.strip('"').strip("'")
-        else:
-            return key, value
-
-        if key in cls.list_fields:
-            list_value = value.split()
-            return key, list_value
-
-        return key, value
-
-    @classmethod
-    def get_config(cls, config_path: str = None) -> CONFIG_FORMAT:
-        config_path = config_path or cls.default_config_path
-        if not cls._cached_config:
-            cls._cached_config = {}
-        if not cls._cached_config.get(config_path):
-            with open_file(config_path) as config_file:
-                cls._cached_config[config_path] = {
-                    key: value
-                    for key, value in [
-                        cls._parse_line(line)
-                        for line in config_file.readlines()
-                    ] if key
-                }
-        return cls._cached_config[config_path]
-
-    @classmethod
-    def get(cls, key: str, fallback: Any = None, config_path: str = None) -> Any:
-        return cls.get_config(config_path=config_path).get(key) or fallback
 
 
 def remove_dir(dir_path: str) -> None:
--- pikaur/makepkg_config.py
+++ pikaur/makepkg_config.py
@@ -1,8 +1,74 @@
 import os
-from typing import Optional, Any
+from typing import (
+    Dict, Any, List, Tuple, Union, Optional,
+)
 
-from .core import ConfigReader
+from .core import open_file
 from .config import CONFIG_ROOT
+
+
+CONFIG_VALUE_TYPE = Union[None, str, List[str]]
+CONFIG_FORMAT = Dict[str, CONFIG_VALUE_TYPE]
+
+
+class ConfigReader():
+
+    comment_prefixes = ('#', ';')
+
+    _cached_config: Optional[Dict[str, CONFIG_FORMAT]] = None
+    default_config_path: str
+    list_fields: List[str] = []
+    ignored_fields: List[str] = []
+
+    @classmethod
+    def _parse_line(cls, line: str) -> Tuple[Optional[str], CONFIG_VALUE_TYPE]:
+        blank = (None, None, )
+        if line.startswith(' '):
+            return blank
+        if '=' not in line:
+            return blank
+        line = line.strip()
+        for comment_prefix in cls.comment_prefixes:
+            line, *_comments = line.split(comment_prefix)
+
+        key, *values = line.split('=')
+        value = '='.join(values)
+        key = key.strip()
+        value = value.strip()
+
+        if key in cls.ignored_fields:
+            return blank
+
+        if value:
+            value = value.strip('"').strip("'")
+        else:
+            return key, value
+
+        if key in cls.list_fields:
+            list_value = value.split()
+            return key, list_value
+
+        return key, value
+
+    @classmethod
+    def get_config(cls, config_path: str = None) -> CONFIG_FORMAT:
+        config_path = config_path or cls.default_config_path
+        if not cls._cached_config:
+            cls._cached_config = {}
+        if not cls._cached_config.get(config_path):
+            with open_file(config_path) as config_file:
+                cls._cached_config[config_path] = {
+                    key: value
+                    for key, value in [
+                        cls._parse_line(line)
+                        for line in config_file.readlines()
+                    ] if key
+                }
+        return cls._cached_config[config_path]
+
+    @classmethod
+    def get(cls, key: str, fallback: Any = None, config_path: str = None) -> Any:
+        return cls.get_config(config_path=config_path).get(key) or fallback
 
 
 class MakepkgConfig():

237ad6e54b7c99f24f409c0488d9a2527a377881
actionless <actionless.loveless@gmail.com>
2018-06-13 05:12:57 +0200

    chore(vulture): start maintaining whitelist for false alarms

--- maintenance_scripts/vulture.sh
+++ maintenance_scripts/vulture.sh
@@ -3,4 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-vulture pikaur/ --exclude argparse.py --sort-by-size --min-confidence=1
+exec vulture pikaur/ \
+	--min-confidence=1 \
+	--sort-by-size \
+	--exclude argparse.py \
+	./maintenance_scripts/vulture_whitelist.py
---
+++ maintenance_scripts/vulture_whitelist.py
@@ -0,0 +1,9 @@
+from vulture.whitelist_utils import Whitelist
+
+whitelist = Whitelist()
+whitelist.deps_destination
+
+whitelist_news = Whitelist()
+whitelist_news.strict
+whitelist_news.convert_charrefs
+whitelist_news.handle_data

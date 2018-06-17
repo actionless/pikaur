233b53e13e8aeced93dbc46c8a4b17fa8da89769
actionless <actionless.loveless@gmail.com>
2018-06-17 08:00:44 +0200

    chore(vulture): update whitelist

--- maintenance_scripts/vulture_whitelist.py
+++ maintenance_scripts/vulture_whitelist.py
@@ -1,9 +1,19 @@
 from vulture.whitelist_utils import Whitelist
 
 whitelist = Whitelist()
-whitelist.deps_destination
 
-whitelist_news = Whitelist()
-whitelist_news.strict
-whitelist_news.convert_charrefs
-whitelist_news.handle_data
+whitelist.build.PackageBuild._get_deps.deps_destination
+
+whitelist.news.MLStripper.strict
+whitelist.news.MLStripper.convert_charrefs
+whitelist.news.MLStripper.handle_data
+
+whitelist.aur.AURPackageInfo.optdepends
+whitelist.aur.AURPackageInfo.id
+whitelist.aur.AURPackageInfo.packagebaseid
+whitelist.aur.AURPackageInfo.maintainer
+whitelist.aur.AURPackageInfo.firstsubmitted
+whitelist.aur.AURPackageInfo.lastmodified
+whitelist.aur.AURPackageInfo.urlpath
+whitelist.aur.AURPackageInfo.license
+whitelist.aur.AURPackageInfo.keywords

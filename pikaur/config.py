b13f220e043dd1c5b6d20886d0205df7d6b4d83e
actionless <actionless.loveless@gmail.com>
2018-06-17 22:38:50 +0200

    chore: bump version to 0.14.3

--- pikaur/config.py
+++ pikaur/config.py
@@ -6,7 +6,7 @@ from typing import Union
 from .core import running_as_root, open_file
 
 
-VERSION = '0.14.2-dev'
+VERSION = '0.14.3-dev'
 
 _USER_CACHE_HOME = os.environ.get(
     "XDG_CACHE_HOME",

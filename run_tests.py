46361413e81500307d84a35cfab001e7739b4200
actionless <actionless.loveless@gmail.com>
2018-06-18 18:53:36 +0200

    test: add for -Ssq <aur> and unknown args

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import sys
 
-from pikaur.main import main
+from pikaur_test.helpers import pikaur, assert_installed
 
 
 WRITE_DB = False
@@ -8,23 +8,40 @@ if (len(sys.argv) > 1) and sys.argv[1] == '--write-db':
     WRITE_DB = True
 
 
-def pikaur(cmd):
-    sys.argv = ['pikaur'] + cmd.split(' ') + (
-        ['--noconfirm'] if '-S' in cmd else []
-    )
-    main()
+# just run info commands for coverage:
+assert(
+    not pikaur('-V').returncode
+)
+assert(
+    not pikaur('-Sh').returncode
+)
+assert(
+    not pikaur('-Qh').returncode
+)
 
 
-pikaur('-V')
+# unknown argument passed to pacman
+assert(
+    pikaur('-Zyx').returncode == 1
+)
 
-pikaur('-Sh')
-pikaur('-Qh')
+
+# search aur packages
+assert(
+    sorted(pikaur('-Ssq oomox').stdout.splitlines()) == ['oomox', 'oomox-git']
+)
 
 
+# tests which are modifying local package DB:
 if WRITE_DB:
 
     # repo package
     pikaur('-S flac')
+    assert_installed('flac')
 
     # aur package
     pikaur('-S inxi')
+    assert_installed('inxi')
+
+
+print('\n\n[OK] All tests passed\n')

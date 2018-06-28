c8d0288292421dcc9965bd6f369e3d24016c92a4
actionless <actionless.loveless@gmail.com>
2018-06-28 16:52:08 +0200

    fix(install cli): nothing to do message when installing package via provided name

--- pikaur/install_cli.py
+++ pikaur/install_cli.py
@@ -228,6 +228,7 @@ class InstallPackagesCLI():
         # check if we really need to build/install anything
         if not (
                 self.install_info.repo_packages_install_info or
+                self.install_info.new_repo_deps_install_info or
                 self.install_info.thirdparty_repo_packages_install_info or
                 self.install_info.aur_updates_install_info
         ):

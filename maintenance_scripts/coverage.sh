8039b4cec1785790c94f0be40d3ee9448a41edf2
actionless <actionless.loveless@gmail.com>
2018-07-21 20:11:51 +0200

    chore(ci): print test names

--- maintenance_scripts/coverage.sh
+++ maintenance_scripts/coverage.sh
@@ -16,7 +16,7 @@ echo "PKGEXT='.pkg.tar'" >> ~/.makepkg.conf
 if [[ "${2:-}" == "--write-db" ]] ; then
 	export WRITE_DB=True
 fi
-coverage run --source=pikaur -m unittest
+coverage run --source=pikaur -m unittest -v
 
 if [[ "${1:-}" == "--coveralls" ]] ; then
 	coveralls

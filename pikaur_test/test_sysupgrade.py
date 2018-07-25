8b74cab87d9581868095353633422bcec0d85ab9
actionless <actionless.loveless@gmail.com>
2018-07-26 01:49:41 +0200

    refactor(tests: sysupgrade): use helper-methods for downgrading the packages

--- pikaur_test/test_sysupgrade.py
+++ pikaur_test/test_sysupgrade.py
@@ -7,56 +7,65 @@ from pikaur_test.helpers import (
     pikaur, spawn,
 )
 
+from pikaur.pacman import PackageDB  # pylint: disable=no-name-in-module
+
 
 class SysupgradeTest(PikaurDbTestCase):
     """
     sysupgrade-related test cases
     """
 
+    repo_pkg_name = 'tree'
+    repo_old_version: str
+
+    aur_pkg_name = 'inxi'
+    aur_old_version: str
+
+    def setUp(self):
+        # just update to make sure everything is on the latest version,
+        # except for test subject packages
+        pikaur('-Syu --noconfirm')
+
+    def downgrade_repo_pkg(self):
+        self.remove_if_installed(self.repo_pkg_name)
+        pikaur(f'-G -d {self.repo_pkg_name}')
+        some_older_commit = spawn(
+            f'git -C ./{self.repo_pkg_name} log --format=%h'
+        ).stdout_text.splitlines()[10]
+        spawn(f'git -C ./{self.repo_pkg_name} checkout {some_older_commit}')
+        pikaur(f'-P -i --noconfirm --mflags=--skippgpcheck '
+               f'./{self.repo_pkg_name}/trunk/PKGBUILD')
+        self.assertInstalled(self.repo_pkg_name)
+        self.repo_old_version = PackageDB.get_local_dict()[self.repo_pkg_name].version
+
+    def downgrade_aur_pkg(self):
+        self.remove_if_installed(self.aur_pkg_name)
+        pikaur(f'-G -d {self.aur_pkg_name}')
+        prev_commit = spawn(
+            f'git -C ./{self.aur_pkg_name} log --format=%h'
+        ).stdout_text.splitlines()[1]
+        spawn(f'git -C ./{self.aur_pkg_name} checkout {prev_commit}')
+        pikaur(f'-P -i --noconfirm ./{self.aur_pkg_name}/PKGBUILD')
+        self.assertInstalled(self.aur_pkg_name)
+        self.aur_old_version = PackageDB.get_local_dict()[self.aur_pkg_name].version
+
     def test_syu(self):
         """
         test upgrade of repo and AUR packages
         and also -U, -P and -G (during downgrading)
         """
-        from pikaur.pacman import PackageDB  # pylint: disable=no-name-in-module
 
-        # just update to make sure everything is on the latest version,
-        # except for test subject packages
-        pikaur('-Syu --noconfirm')
+        self.downgrade_repo_pkg()
+        self.downgrade_aur_pkg()
 
-        # repo package downgrade
-        repo_pkg_name = 'tree'
-        pikaur(f'-G -d {repo_pkg_name}')
-        some_older_commit = spawn(
-            f'git -C ./{repo_pkg_name} log --format=%h'
-        ).stdout_text.splitlines()[10]
-        spawn(f'git -C ./{repo_pkg_name} checkout {some_older_commit}')
-        pikaur(f'-P -i --noconfirm --mflags=--skippgpcheck '
-               f'./{repo_pkg_name}/trunk/PKGBUILD')
-        self.assertInstalled(repo_pkg_name)
-        repo_old_version = PackageDB.get_local_dict()[repo_pkg_name].version
-
-        # AUR package downgrade
-        aur_pkg_name = 'inxi'
-        self.remove_if_installed(aur_pkg_name)
-        pikaur(f'-G -d {aur_pkg_name}')
-        prev_commit = spawn(
-            f'git -C ./{aur_pkg_name} log --format=%h'
-        ).stdout_text.splitlines()[1]
-        spawn(f'git -C ./{aur_pkg_name} checkout {prev_commit}')
-        pikaur(f'-P -i --noconfirm ./{aur_pkg_name}/PKGBUILD')
-        self.assertInstalled(aur_pkg_name)
-        aur_old_version = PackageDB.get_local_dict()[aur_pkg_name].version
-
-        # test pikaur -Qu
         query_result = pikaur('-Quq --aur').stdout.strip()
         self.assertEqual(
-            query_result, aur_pkg_name
+            query_result, self.aur_pkg_name
         )
 
         query_result = pikaur('-Quq --repo').stdout.strip()
         self.assertEqual(
-            query_result, repo_pkg_name
+            query_result, self.repo_pkg_name
         )
 
         query_result = pikaur('-Qu').stdout
@@ -64,17 +73,19 @@ class SysupgradeTest(PikaurDbTestCase):
             len(query_result.splitlines()), 2
         )
         self.assertIn(
-            aur_pkg_name, query_result
+            self.aur_pkg_name, query_result
         )
         self.assertIn(
-            repo_pkg_name, query_result
+            self.repo_pkg_name, query_result
         )
 
         # and finally test the sysupgrade itself
         pikaur('-Syu --noconfirm')
         self.assertNotEqual(
-            PackageDB.get_local_dict()[repo_pkg_name].version, repo_old_version
+            PackageDB.get_local_dict()[self.repo_pkg_name].version,
+            self.repo_old_version
         )
         self.assertNotEqual(
-            PackageDB.get_local_dict()[aur_pkg_name].version, aur_old_version
+            PackageDB.get_local_dict()[self.aur_pkg_name].version,
+            self.aur_old_version
         )

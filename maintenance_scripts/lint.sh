1b661d7896156ad8649a29ddf54054f22153c234
actionless <actionless.loveless@gmail.com>
2018-07-25 07:05:30 +0200

    chore(lint): more verbose output

--- maintenance_scripts/lint.sh
+++ maintenance_scripts/lint.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo Flake8:
 flake8 pikaur pikaur.py pikaur_test
+
+echo PyLint:
 pylint pikaur.py pikaur pikaur_test
+
+echo MyPy:
 mypy --ignore-missing-imports pikaur.py pikaur_test
+
+echo Vulture:
 ./maintenance_scripts/vulture.sh
+
+echo == GOOD!

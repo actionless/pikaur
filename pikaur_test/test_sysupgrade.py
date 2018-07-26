09ae12bee101c8fddba8ef10bf7022ebfea4a1ba
actionless <actionless.loveless@gmail.com>
2018-07-26 02:31:38 +0200

    test(sysupgrade): cover --devel

--- pikaur_test/test_sysupgrade.py
+++ pikaur_test/test_sysupgrade.py
@@ -21,6 +21,10 @@ class SysupgradeTest(PikaurDbTestCase):
     aur_pkg_name = 'inxi'
     aur_old_version: str
 
+    dev_pkg_name = 'termdown-git'
+    dev_pkg_url = "https://github.com/trehn/termdown"
+    dev_old_version: str
+
     def setUp(self):
         # just update to make sure everything is on the latest version,
         # except for test subject packages
@@ -28,7 +32,7 @@ class SysupgradeTest(PikaurDbTestCase):
 
     def downgrade_repo_pkg(self):
         self.remove_if_installed(self.repo_pkg_name)
-        pikaur(f'-G -d {self.repo_pkg_name}')
+        pikaur(f'-G {self.repo_pkg_name}')
         some_older_commit = spawn(
             f'git -C ./{self.repo_pkg_name} log --format=%h'
         ).stdout_text.splitlines()[10]
@@ -39,8 +43,9 @@ class SysupgradeTest(PikaurDbTestCase):
         self.repo_old_version = PackageDB.get_local_dict()[self.repo_pkg_name].version
 
     def downgrade_aur_pkg(self):
+        # test -P and -G during downgrading
         self.remove_if_installed(self.aur_pkg_name)
-        pikaur(f'-G -d {self.aur_pkg_name}')
+        pikaur(f'-G {self.aur_pkg_name}')
         prev_commit = spawn(
             f'git -C ./{self.aur_pkg_name} log --format=%h'
         ).stdout_text.splitlines()[1]
@@ -49,10 +54,48 @@ class SysupgradeTest(PikaurDbTestCase):
         self.assertInstalled(self.aur_pkg_name)
         self.aur_old_version = PackageDB.get_local_dict()[self.aur_pkg_name].version
 
+    def downgrade_dev_pkg(self):
+        # test -P <custom_name> and -G -d during downgrading
+        self.remove_if_installed(self.dev_pkg_name)
+        pikaur(f'-G -d {self.dev_pkg_name}')
+        dev_pkg_url = self.dev_pkg_url.replace('/', r'\/')
+        spawn([
+            "bash",
+            "-c",
+            f"cd ./{self.dev_pkg_name}/ && "
+            "sed -e 's/"
+            "^source=.*"
+            "/"
+            f'source=("git+{dev_pkg_url}#branch=master~1")'
+            "/' PKGBUILD > PKGBUILD_prev"
+        ])
+        pikaur(f'-P -i --noconfirm ./{self.dev_pkg_name}/PKGBUILD_prev')
+        self.assertInstalled(self.dev_pkg_name)
+        self.dev_old_version = PackageDB.get_local_dict()[self.dev_pkg_name].version
+
+    def test_devel_upgrade(self):
+        """
+        test upgrade of AUR dev package
+        """
+
+        self.downgrade_dev_pkg()
+
+        query_result = pikaur('-Qu').stdout
+        self.assertEqual(
+            len([l for l in query_result.splitlines() if l]), 0
+        )
+
+        # and finally test the sysupgrade itself
+        pikaur('-Su --noconfirm --devel')
+        # pikaur(f'-S {self.dev_pkg_name} --noconfirm --devel')
+        self.assertNotEqual(
+            PackageDB.get_local_dict()[self.dev_pkg_name].version,
+            self.dev_old_version
+        )
+
     def test_syu(self):
         """
         test upgrade of repo and AUR packages
-        and also -U, -P and -G (during downgrading)
         """
 
         self.downgrade_repo_pkg()

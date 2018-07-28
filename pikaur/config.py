8342f7d9a1671405689397484ee6851f885a1d3a
actionless <actionless.loveless@gmail.com>
2018-07-28 19:29:33 +0200

    chore: bump version to 1.2.9

--- pikaur/config.py
+++ pikaur/config.py
@@ -9,7 +9,7 @@ from typing import Dict, Optional
 from .core import running_as_root, open_file
 
 
-VERSION = '1.2.8-dev'
+VERSION = '1.2.9-dev'
 
 _USER_CACHE_HOME = os.environ.get(
     "XDG_CACHE_HOME",

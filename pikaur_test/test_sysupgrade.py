2a822a747ac2045e0137db555ec51035ba2dd714
actionless <actionless.loveless@gmail.com>
2018-07-29 09:11:51 +0200

    test(sysupgrade): try using smaller packages which would build faster - 4

--- pikaur_test/test_sysupgrade.py
+++ pikaur_test/test_sysupgrade.py
@@ -15,16 +15,16 @@ class SysupgradeTest(PikaurDbTestCase):
     sysupgrade-related test cases
     """
 
-    repo_pkg_name = 'tree'
+    repo2_pkg_name = 'expac'
     repo_old_version: str
 
-    aur_pkg_name = 'etc-update'
+    aur2_pkg_name = 'pkgbuild-introspection'
     aur_old_version: str
 
-    repo2_pkg_name = 'expac'
+    repo_pkg_name = 'tree'
     repo2_old_version: str
 
-    aur2_pkg_name = 'git-hub'
+    aur_pkg_name = 'etc-update'
     aur2_old_version: str
 
     dev_pkg_name = 'pxltrm-git'

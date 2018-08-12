21b816a8f5a1413157d29d4ca5377cfdacc2c0c2
actionless <actionless.loveless@gmail.com>
2018-08-12 20:06:32 +0200

    chore: bump version to 1.2.20

--- pikaur/config.py
+++ pikaur/config.py
@@ -9,7 +9,7 @@ from typing import Dict, Optional
 from .core import running_as_root, open_file
 
 
-VERSION = '1.2.19-dev'
+VERSION = '1.2.20-dev'
 
 _USER_CACHE_HOME = os.environ.get(
     "XDG_CACHE_HOME",

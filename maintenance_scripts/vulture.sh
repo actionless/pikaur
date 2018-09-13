099140b7ef555ae9d5f7ec585ed36da48b57b726
actionless <actionless.loveless@gmail.com>
2018-09-13 16:50:28 +0200

    fix(maintenance_scripts: vulture): compatibility with 0.29

--- maintenance_scripts/vulture.sh
+++ maintenance_scripts/vulture.sh
@@ -5,6 +5,6 @@ IFS=$'\n\t'
 
 	#--exclude argparse.py \
 exec vulture pikaur/ pikaur_test/ \
+	./maintenance_scripts/vulture_whitelist.py \
 	--min-confidence=1 \
-	--sort-by-size \
-	./maintenance_scripts/vulture_whitelist.py
+	--sort-by-size

75e2e5ad83f9b6b1f1ee5a06be81e0d721bccbd6
actionless <actionless.loveless@gmail.com>
2018-12-31 03:06:05 +0100

    chore(maintenance_scripts: lint): don't print pylint score

--- maintenance_scripts/lint.sh
+++ maintenance_scripts/lint.sh
@@ -5,7 +5,7 @@ echo Flake8:
 flake8 pikaur pikaur.py pikaur_test
 
 echo PyLint:
-pylint pikaur.py pikaur pikaur_test
+pylint pikaur.py pikaur pikaur_test --score no
 
 echo MyPy:
 env MYPYPATH=./maintenance_scripts/mypy_stubs mypy pikaur.py pikaur_test

043a5556a2651c2f4fdae3f2c32fac5a4af5284a
actionless <actionless.loveless@gmail.com>
2019-09-09 04:32:28 +0200

    chore: bump version to 1.4.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pikaur',  # Required
-    version='1.4.3',  # Required
+    version='1.4.4',  # Required
     description='AUR helper with minimal dependencies',  # Required
     long_description=long_description,  # Optional
     url='https://github.com/actionless/pikaur',  # Optional

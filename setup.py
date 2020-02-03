ae59f3c96d8164ddc2c4657b4de536b91f573d70
actionless <actionless.loveless@gmail.com>
2020-02-03 18:11:33 +0100

    fix(setup.py): don't add extra args for setuptools unless setuptools is used (fixes #450)

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 # To use a consistent encoding
 import codecs
 from os import path, environ
+from typing import Dict, Any
 
 if environ.get('PYPY_BUILD'):
     import setuptools  # pylint: disable=unused-import
@@ -13,12 +14,11 @@ HERE = path.abspath(path.dirname(__file__))
 with codecs.open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
 
-setup(
+SETUP_ARGS: Dict[str, Any] = dict(
     name='pikaur',  # Required
     version='1.6.1',  # Required
     description='AUR helper with minimal dependencies',  # Required
     long_description=LONG_DESCRIPTION,  # Optional
-    long_description_content_type="text/markdown",
     url='https://github.com/actionless/pikaur',  # Optional
     author='Yauheni Kirylau',  # Optional
     author_email='actionless.loveless@gmail.com',  # Optional
@@ -49,13 +49,20 @@ setup(
     # Note that this is a string of words separated by whitespace, not a list.
     keywords='arch linux aur helper',
 
-    install_requires=["pyalpm"],
-
     packages=["pikaur", ],  # Required
+)
 
+EXTRA_ARGS_SETUPTOOLS = dict(
+    long_description_content_type="text/markdown",
+    install_requires=["pyalpm"],
     entry_points={
         'console_scripts': [
             'pikaur = pikaur.main:main'
         ]
     }
 )
+
+if environ.get('PYPY_BUILD'):
+    SETUP_ARGS.update(EXTRA_ARGS_SETUPTOOLS)
+
+setup(**SETUP_ARGS)

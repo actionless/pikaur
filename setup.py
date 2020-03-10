2760dea5c6f715363de42252d8cb2e215eefb821
actionless <actionless.loveless@gmail.com>
2020-03-10 16:45:20 +0100

    chore: bump version to 1.6.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with codecs.open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 SETUP_ARGS: Dict[str, Any] = dict(
     name='pikaur',  # Required
-    version='1.6.6',  # Required
+    version='1.6.7',  # Required
     description='AUR helper with minimal dependencies',  # Required
     long_description=LONG_DESCRIPTION,  # Optional
     url='https://github.com/actionless/pikaur',  # Optional

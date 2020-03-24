2afd663b885c5f0a6de7beefd6171bca23ab677b
actionless <actionless.loveless@gmail.com>
2020-03-24 12:58:19 +0100

    chore(ci): add python compile early check

--- maintenance_scripts/lint.sh
+++ maintenance_scripts/lint.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo Flake8:
+echo Python compile...
+python3 -O -m compileall pikaur pikaur.py pikaur_test | (grep -v -e '^Listing' -e '^Compiling' || true)
+
+echo Flake8...
 flake8 pikaur pikaur.py pikaur_test
 
 echo PyLint...
 python -m pylint --jobs="$(nproc)"  pikaur.py pikaur pikaur_test --score no
 
-echo MyPy:
+echo MyPy...
 env MYPYPATH=./maintenance_scripts/mypy_stubs python -m mypy pikaur.py pikaur_test
 
-echo Vulture:
+echo Vulture...
 ./maintenance_scripts/vulture.sh
 
 echo Shellcheck...

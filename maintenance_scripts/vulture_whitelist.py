e770283387ea3a44f6501038ce91c27768695349
actionless <actionless.loveless@gmail.com>
2020-03-30 13:52:21 +0200

    chore(vulture): update whitelist

--- maintenance_scripts/vulture_whitelist.py
+++ maintenance_scripts/vulture_whitelist.py
@@ -9,16 +9,16 @@ whitelist = Whitelist()
 
 whitelist.argparse.ArgumentParserWithUnknowns._parse_known_args
 
-whitelist.aur.AURPackageInfo.firstsubmitted
-whitelist.aur.AURPackageInfo.git_url
-whitelist.aur.AURPackageInfo.id
-whitelist.aur.AURPackageInfo.keywords
-whitelist.aur.AURPackageInfo.lastmodified
-whitelist.aur.AURPackageInfo.license
-whitelist.aur.AURPackageInfo.maintainer
 whitelist.aur.AURPackageInfo.optdepends
+whitelist.aur.AURPackageInfo.id
 whitelist.aur.AURPackageInfo.packagebaseid
+whitelist.aur.AURPackageInfo.maintainer
+whitelist.aur.AURPackageInfo.firstsubmitted
+whitelist.aur.AURPackageInfo.lastmodified
 whitelist.aur.AURPackageInfo.urlpath
+whitelist.aur.AURPackageInfo.license
+whitelist.aur.AURPackageInfo.keywords
+whitelist.aur.AURPackageInfo.git_url
 
 whitelist.build.PackageBuild._get_deps.deps_destination
 
@@ -27,8 +27,3 @@ whitelist.main.socket.socket
 whitelist.news.MLStripper.convert_charrefs
 whitelist.news.MLStripper.handle_data
 whitelist.news.MLStripper.strict
-
-# @TODO: will be used for resolving install deps after editing PKGBUILD by user:
-whitelist.SrcInfo.get_depends
-whitelist.SrcInfo.get_makedepends
-whitelist.SrcInfo.get_checkdepends

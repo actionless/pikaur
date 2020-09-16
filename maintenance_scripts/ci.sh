63bfb98e742b74659442ca14fbc935c9aa2233ff
actionless <actionless.loveless@gmail.com>
2020-09-16 19:24:46 +0200

    chore(ci): disable network connection test as it is now

--- maintenance_scripts/ci.sh
+++ maintenance_scripts/ci.sh
@@ -4,10 +4,10 @@ IFS=$'\n\t'
 
 cd "$(readlink -e "$(dirname "${0}")")"/..
 
-ping 8.8.8.8 -c 1 || {
-	echo "No internet connection"
-	exit 1
-}
+#ping 8.8.8.8 -c 1 || {
+#    echo "No internet connection"
+#    exit 1
+#}
 
 ./maintenance_scripts/lint.sh
 ./maintenance_scripts/coverage.sh "${1:---local}" "${2:---write-db}"

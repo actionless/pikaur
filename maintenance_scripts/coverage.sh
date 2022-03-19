df46c1af232cbecfaf84117b1ca3372885266d5f
actionless <actionless.loveless@gmail.com>
2022-03-19 18:42:42 +0100

    fix(maintenance_scripts: coverage): dont touch ~/.makepkg.conf unless write mode chosen

--- maintenance_scripts/coverage.sh
+++ maintenance_scripts/coverage.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-export PATH="${PATH}:/usr/bin/core_perl"
-echo "PKGEXT='.pkg.tar'" >> ~/.makepkg.conf
-
 if [[ "${2:-}" == "--write-db" ]] ; then
 	export WRITE_DB=True
+	export PATH="${PATH}:/usr/bin/core_perl"
+	echo "PKGEXT='.pkg.tar'" >> ~/.makepkg.conf
 fi
+
 coverage run --source=pikaur -m unittest -v
 
 if [[ "${1:-}" == "--coveralls" ]] ; then

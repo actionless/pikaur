c48c41b815648a1688de82bc484733114ceb7e54
actionless <actionless.loveless@gmail.com>
2022-06-09 07:22:28 +0200

    test(pikspect): add some basic test coverage

---
+++ pikaur_test/test_unit_pikspect.py
@@ -0,0 +1,21 @@
+""" This file is licensed under GPLv3, see https://www.gnu.org/licenses/ """
+from pikaur_test.helpers import PikaurTestCase
+from pikaur.pikspect import pikspect
+
+
+class PikspectTest(PikaurTestCase):
+
+    def test_error_argstype(self):
+        with self.assertRaises(TypeError):
+            pikspect(cmd='echo')
+
+    def test_basic(self):
+        result = pikspect(cmd=['echo', 'test'])
+        self.assertEqual(result.output, b'')
+        result = pikspect(cmd=['echo', 'test'], capture_output=True)
+        self.assertEqual(result.output, b'test\r\n')
+        result = pikspect(
+            cmd=['bash', '-c', 'echo foo ; sleep 0.1 ; echo bar'],
+            capture_output=True
+        )
+        self.assertEqual(result.output, b'foo\r\nbar\r\n')

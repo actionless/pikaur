53190e00e521114ecab674ff2cb9425e199a92ca
actionless <actionless.loveless@gmail.com>
2022-11-26 06:33:00 +0100

    test(unit: main): cover the ascii encoding too

--- pikaur_test/test_unit_main.py
+++ pikaur_test/test_unit_main.py
@@ -2,6 +2,7 @@
 # mypy: disable-error-code=no-untyped-def
 
 import sys
+from unittest import mock
 
 from pikaur.main import OutputEncodingWrapper
 from pikaur_test.helpers import PikaurTestCase
@@ -12,32 +13,40 @@ class MainHelperFuncsTest(PikaurTestCase):
     def test_output_encoding_wrapper(self):
         real_stdout = sys.stdout
         real_stderr = sys.stderr
-        print(1)
         with OutputEncodingWrapper():
-            print('test')
+            # self.assertNotEqual(real_stderr, sys.stderr)
 
             # fails if testrunner (like nosetests) capturing output:
             # self.assertNotEqual(real_stdout, sys.stdout)
 
-            # self.assertNotEqual(real_stderr, sys.stderr)
-            print(2)
-        print(3)
+            print('test')
         self.assertEqual(real_stdout, sys.stdout)
         self.assertEqual(real_stderr, sys.stderr)
 
     def test_output_encoding_wrapper_2(self):
         real_stdout = sys.stdout
         real_stderr = sys.stderr
-        print(1)
         with self.assertRaises(SystemExit):
             with OutputEncodingWrapper():
-                print('test')
+                # self.assertNotEqual(real_stderr, sys.stderr)
 
                 # fails if testrunner (like nosetests) capturing output:
                 # self.assertNotEqual(real_stdout, sys.stdout)
 
-                # self.assertNotEqual(real_stderr, sys.stderr)
                 raise Exception('test')
-        print(3)
         self.assertEqual(real_stdout, sys.stdout)
         self.assertEqual(real_stderr, sys.stderr)
+
+    def test_output_encoding_wrapper_ascii(self):
+        with mock.patch('pikaur.main.DEFAULT_INPUT_ENCODING', new='ascii'):
+            real_stdout = sys.stdout
+            real_stderr = sys.stderr
+            with OutputEncodingWrapper():
+                # self.assertNotEqual(real_stderr, sys.stderr)
+
+                # fails if testrunner (like nosetests) capturing output:
+                # self.assertNotEqual(real_stdout, sys.stdout)
+
+                print('test')
+            self.assertEqual(real_stdout, sys.stdout)
+            self.assertEqual(real_stderr, sys.stderr)

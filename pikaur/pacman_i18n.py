cb6bb270bd8a038d5cde6e1da8671f95ac4902be
actionless <actionless.loveless@gmail.com>
2022-12-12 21:31:09 +0100

    refactor(pacman): don't use global vars for pacman's i18n

--- pikaur/pacman_i18n.py
+++ pikaur/pacman_i18n.py
@@ -1,7 +1,16 @@
 import gettext
 
-PACMAN_TRANSLATION = gettext.translation('pacman', fallback=True)
+
+class PacmanTranslation():
+
+    translation: gettext.NullTranslations | None = None
+
+    @classmethod
+    def get(cls) -> gettext.NullTranslations:
+        if not cls.translation:
+            cls.translation = gettext.translation('pacman', fallback=True)
+        return cls.translation
 
 
 def _p(msg: str) -> str:
-    return PACMAN_TRANSLATION.gettext(msg)
+    return PacmanTranslation.get().gettext(msg)

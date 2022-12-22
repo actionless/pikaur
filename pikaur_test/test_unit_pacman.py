754c410b5518acc8bc22e3c9643cc5a4f288f13b
actionless <actionless.loveless@gmail.com>
2022-12-22 10:53:17 +0100

    test(unit: pacman): add for 2 methods in PackageDB

---
+++ pikaur_test/test_unit_pacman.py
@@ -0,0 +1,16 @@
+"""Licensed under GPLv3, see https://www.gnu.org/licenses/"""
+# mypy: disable-error-code=no-untyped-def
+
+from pikaur.pacman import PackageDB, RepositoryNotFoundError
+from pikaur_test.helpers import PikaurTestCase
+
+
+class PacmanTestCase(PikaurTestCase):
+
+    def test_error_item_bool_get_str(self):
+        with self.assertRaises(RepositoryNotFoundError):
+            PackageDB.get_repo_priority(repo_name="some_funny_repo_name")
+
+    def test_find_repo_package_multiple_providers(self):
+        found_pkg = PackageDB.find_repo_package("java-runtime")
+        self.assertIn("jre-openjdk", found_pkg.name)

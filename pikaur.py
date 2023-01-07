911929396e4a57ffe494d737fd4a6469da56088c
actionless <actionless.loveless@gmail.com>
2023-01-07 05:41:44 +0100

    style(pikaur.py): ruff

--- pikaur.py
+++ pikaur.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-""" Licensed under GPLv3, see https://www.gnu.org/licenses/ """
+"""Licensed under GPLv3, see https://www.gnu.org/licenses/"""
 
 from pikaur.main import main
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

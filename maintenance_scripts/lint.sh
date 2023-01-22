1e85c39ed5cf010bc51674def4371265273647e2
actionless <actionless.loveless@gmail.com>
2023-01-22 23:35:53 +0100

    refactor(maintenance_scripts: lint): use same python executable for every command

--- maintenance_scripts/lint.sh
+++ maintenance_scripts/lint.sh
@@ -14,16 +14,17 @@ if [[ -n "${1:-}" ]] ; then
 fi
 
 export PYTHONWARNINGS='default,error:::pikaur[.*],error:::pikaur_test[.*]'
+PYTHON=python3
 
 echo Python compile...
-python3 -O -m compileall "${TARGETS[@]}" \
+"$PYTHON" -O -m compileall "${TARGETS[@]}" \
 | (\
 	grep -v -e '^Listing' -e '^Compiling' || true \
 )
 
 echo Python import...
-#python3 -c "import pikaur"
-python3 -c "import pikaur.main"
+#"$PYTHON" -c "import pikaur"
+"$PYTHON" -c "import pikaur.main"
 
 echo Checking for non-Final globals...
 ./maintenance_scripts/get_non_final_expressions.sh
@@ -33,34 +34,34 @@ echo Checking for unreasonable global vars...
 
 echo Ruff...
 if [[ ! -f "${APP_DIR}/env/bin/activate" ]] ; then
-	python -m venv "${APP_DIR}/env" --system-site-packages
+	"$PYTHON" -m venv "${APP_DIR}/env" --system-site-packages
 	# shellcheck disable=SC1091
 	. "${APP_DIR}/env/bin/activate"
-	pip install ruff --upgrade
+	"$PYTHON" -m pip install ruff --upgrade
 	deactivate
 fi
 "${APP_DIR}/env/bin/ruff" "${TARGETS[@]}"
 
 echo Flake8...
-flake8 "${TARGETS[@]}"
+"$PYTHON" -m flake8 "${TARGETS[@]}"
 
 echo PyLint...
-#python -m pylint --jobs="$(nproc)" "${TARGETS[@]}" --score no 2>&1 \
+#"$PYTHON" -m pylint --jobs="$(nproc)" "${TARGETS[@]}" --score no
 # @TODO: --jobs is broken at the moment: https://github.com/PyCQA/pylint/issues/374
-python -m pylint "${TARGETS[@]}" --score no
+"$PYTHON" -m pylint "${TARGETS[@]}" --score no
 
 echo MyPy...
-python -m mypy "${TARGETS[@]}" --no-error-summary
+"$PYTHON" -m mypy "${TARGETS[@]}" --no-error-summary
 
 echo Vulture...
 	#--exclude argparse.py \
-vulture "${TARGETS[@]}" \
+"$PYTHON" -m vulture "${TARGETS[@]}" \
 	./maintenance_scripts/vulture_whitelist.py \
 	--min-confidence=1 \
 	--sort-by-size
 
 echo Bandit...
-bandit "${TARGETS[@]}" --recursive --silent
+"$PYTHON" -m bandit "${TARGETS[@]}" --recursive --silent
 
 echo Shellcheck...
 (
@@ -73,7 +74,7 @@ echo Shellcheck...
 echo Shellcheck Makefile...
 (
 	cd "${APP_DIR}"
-	./maintenance_scripts/makefile_shellcheck.py
+	"$PYTHON" ./maintenance_scripts/makefile_shellcheck.py
 )
 
 echo Validate pyproject file...

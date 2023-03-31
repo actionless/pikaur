badaa4ef13797619e5bcd79743622c97c9668951
actionless <actionless.loveless@gmail.com>
2023-03-31 04:06:27 +0200

    fix(maintenance_scripts: docker_push): add --ulimit to workaround fakeroot hanging on buildx

--- maintenance_scripts/docker_push.sh
+++ maintenance_scripts/docker_push.sh
@@ -3,6 +3,6 @@ set -ue
 
 tag=${1:-$(date "+%Y%m%d")}
 
-docker build . --pull -t actionless/pikaur:"$tag" -f Dockerfile_export
+docker build . --pull -t actionless/pikaur:"$tag" -f Dockerfile_export --ulimit nofile=1024:524288
 docker tag actionless/pikaur:"$tag" actionless/pikaur:latest
 docker push actionless/pikaur:latest

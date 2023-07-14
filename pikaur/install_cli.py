actionless/pikaur

commit c2bd34083033da4f3ca0c0fe424ebdb092dca2c8

refactor(install_cli): extract refresh-related stuff into separate method

pikaur/install_cli.py
@@ -156,6 +156,22 @@ class InstallPackagesCLI:
         self.transactions = {}
         self.failed_to_build_package_names = []
 
+        self._handle_refresh()
+
+        if self.args.sysupgrade and not self.args.repo:
+            print_stderr("{} {}".format(  # pylint: disable=consider-using-f-string
+                color_line("::", ColorsHighlight.blue),
+                bold_line(translate("Starting full AUR upgrade...")),
+            ))
+        if self.args.aur:
+            self.not_found_repo_pkgs_names = self.install_package_names
+            self.install_package_names = []
+        if self.args.pkgbuild:
+            self.get_info_from_pkgbuilds()
+
+        self.main_sequence()
+
+    def _handle_refresh(self) -> None:
         if not self.args.aur and (self.args.sysupgrade or self.args.refresh):
 
             with ThreadPool() as pool:
@@ -185,19 +201,6 @@ class InstallPackagesCLI:
             if self.args.refresh:
                 PackageDB.discard_repo_cache()
                 print_stdout()
-
-        if self.args.sysupgrade and not self.args.repo:
-            print_stderr("{} {}".format(  # pylint: disable=consider-using-f-string
-                color_line("::", ColorsHighlight.blue),
-                bold_line(translate("Starting full AUR upgrade...")),
-            ))
-        if self.args.aur:
-            self.not_found_repo_pkgs_names = self.install_package_names
-            self.install_package_names = []
-        if self.args.pkgbuild:
-            self.get_info_from_pkgbuilds()
-
-        self.main_sequence()
 
     class ExitMainSequence(Exception):  # noqa: N818
         """Raise when need to finish Install CLI"""

commit 0e5199e5a8eb5565b8f68c9be4b1352dda2b38f9

fix(install_cli: handle_refresh): regression after c2bd34083033da4f3ca0c0fe424ebdb092dca2c8

pikaur/install_cli.py
@@ -156,7 +156,10 @@ class InstallPackagesCLI:
         self.transactions = {}
         self.failed_to_build_package_names = []
 
-        self._handle_refresh()
+        try:
+            self._handle_refresh()
+        except self.ExitMainSequence:
+            return
 
         if self.args.sysupgrade and not self.args.repo:
             print_stderr("{} {}".format(  # pylint: disable=consider-using-f-string
@@ -196,7 +199,7 @@ class InstallPackagesCLI:
                 sub_tty.restore_new()
 
             if not (self.install_package_names or self.args.sysupgrade):
-                return
+                raise self.ExitMainSequence
 
             if self.args.refresh:
                 PackageDB.discard_repo_cache()
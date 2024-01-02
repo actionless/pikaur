7494e827863e93baac3df78254fa73fac845f271
actionless <actionless.loveless@gmail.com>
2024-01-02 05:00:12 +0100

    style(maintenance_scripts): ruff

--- maintenance_scripts/find_conflicting_aur_pkgs.py
+++ maintenance_scripts/find_conflicting_aur_pkgs.py
@@ -21,13 +21,13 @@ else:
 print("Filtering...\n")
 all_conflicts: dict[str, list[str]] = {}
 for pkg in aur_pkgs:
-    for name in [pkg.name, ]:  # + list(getattr(pkg, "provides", [])):
+    for name in [pkg.name]:  # + list(getattr(pkg, "provides", [])):
         for conf in getattr(pkg, "conflicts", []):
             all_conflicts.setdefault(name, []).append(conf)
 
 # Print all conflicting packages in AUR:
 try:
-    for pkgbase, pkgnames in all_conflicts.items():
+    for pkgbase in all_conflicts:
         for pkgbase_2, pkgnames_2 in all_conflicts.items():
             if pkgbase != pkgbase_2 and pkgbase in pkgnames_2:
                 print(f"{pkgbase}: {pkgbase_2}")

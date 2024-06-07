63bad5678cc767e88e48cee9a3d27edaaf46ae2b
actionless <actionless.loveless@gmail.com>
2024-06-07 08:08:22 +0200

    chore(core: check_runtime_deps): fakeroot is now part of base-devel

--- pikaur/core.py
+++ pikaur/core.py
@@ -361,7 +361,7 @@ def check_runtime_deps(dep_names: list[str] | None = None) -> None:
         sys.exit(65)
     if not dep_names:
         privilege_escalation_tool = PikaurConfig().misc.PrivilegeEscalationTool.get_str()
-        dep_names = ["fakeroot"] + (
+        dep_names = ["base-devel"] + (
             [privilege_escalation_tool] if not RunningAsRoot()() else []
         )
 

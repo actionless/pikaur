52594a158d8e02ebba89458f044d293cb6f59187
actionless <actionless.loveless@gmail.com>
2024-08-27 00:37:46 +0200

    feat(pikasay): add --help

--- pikaur/pikasay.py
+++ pikaur/pikasay.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 from typing import Final
 
@@ -5,6 +6,7 @@ from .pikaprint import (
     format_paragraph,
     get_term_width,
     make_equal_right_padding,
+    print_stdout,
     printable_length,
 )
 
@@ -23,12 +25,17 @@ PIKAPIC: Final = r"""
 """
 
 
-def bubble_top(text: str, padding: int = 1, inner_padding: int = 1) -> str:
+def bubble_top(
+        text: str, margin: int = 1, padding: int = 1, width: int | None = None,
+) -> str:
     bubble_top_left = " _____/|"
     formatted_paragraph = make_equal_right_padding(
         format_paragraph(
-            text, padding=inner_padding, width=get_term_width() - padding * 2 - inner_padding * 2,
-            force=True, split_words=True,
+            text,
+            padding=padding,
+            width=(width or get_term_width()) - margin * 2 - padding * 2,
+            force=True,
+            split_words=True,
         ),
     )
     paragraph_width = printable_length(formatted_paragraph.splitlines()[0])
@@ -38,45 +45,80 @@ def bubble_top(text: str, padding: int = 1, inner_padding: int = 1) -> str:
             formatted_paragraph, max_string_length,
         )
     return "".join((
-        " " * padding,
+        " " * margin,
         bubble_top_left,
         "_" * (max_string_length - len(bubble_top_left) + 1),
-        " " * padding,
+        " " * margin,
         "\n",
 
-        " " * padding,
+        " " * margin,
         "/",
         " " * max_string_length,
         "\\",
-        " " * padding,
+        " " * margin,
         "\n",
 
-        *(f"{' ' * padding}|{line}|{' ' * padding}\n" for line in formatted_paragraph.splitlines()),
+        f"{' ' * margin}|{' ' * max_string_length}|{' ' * margin}\n" * ((padding - 1) // 2),
+        *(f"{' ' * margin}|{line}|{' ' * margin}\n" for line in formatted_paragraph.splitlines()),
+        f"{' ' * margin}|{' ' * max_string_length}|{' ' * margin}\n" * ((padding - 1) // 2),
 
-        " " * padding,
+        " " * margin,
         "\\",
         "_" * max_string_length,
         "/",
-        " " * padding,
-        "\n",
+        " " * margin,
     ))
 
 
-def pikasay(text: str) -> None:
-    sys.stdout.write("".join((
-        PIKAPIC, bubble_top(text),
+def pikasay(
+        text: str, margin: int = 1, padding: int = 1, width: int | None = None,
+) -> None:
+    print_stdout("".join((
+        PIKAPIC, bubble_top(text=text, margin=margin, padding=padding, width=width),
     )))
 
 
 def pikasay_cli() -> None:
-    text = " ".join(sys.argv[1:])
-    if (text in {"-", ""}) or (not sys.stdin.isatty()):
-        if text in {"-", ""}:
-            text = ""
-        else:
+    parser = argparse.ArgumentParser(
+        description="⚡️⚡️PikaSay",
+    )
+    parser.add_argument(
+        "text",
+        nargs="*",
+        help="text to print",
+    )
+    parser.add_argument(
+        "-",
+        dest="stdin",
+        default=(not sys.stdin.isatty()),
+        action="store_true",
+        help="read from stdin",
+    )
+    parser.add_argument(
+        "--margin",
+        type=int,
+        default=1,
+        help="text bubble margin",
+    )
+    parser.add_argument(
+        "--padding",
+        type=int,
+        default=2,
+        help="text bubble padding",
+    )
+    parser.add_argument(
+        "--width",
+        type=int,
+        help="force terminal width",
+    )
+    args = parser.parse_args()
+
+    text = " ".join(args.text)
+    if (not args.text) or args.stdin:
+        if args.text:
             text += "\n"
         text += "\n".join(line.rstrip("\n") for line in sys.stdin.readlines())
-    pikasay(text)
+    pikasay(text, margin=args.margin, padding=args.padding, width=args.width)
 
 
 if __name__ == "__main__":

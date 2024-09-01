a7987eba147786bf0171afc675656417216a4ba3
actionless <actionless.loveless@gmail.com>
2024-09-01 20:44:31 +0200

    chore(pikaur-static: add missing build files and symlinks for building pikaur-static

---
+++ pikaur_static/build-nuitka-new.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+
+function debug
+	echo -e "--> $argv \n"
+	command $argv
+end
+
+debug \
+	nuitka --jobs=(nproc) \
+		--plugin-enable=pylint-warnings \
+		\
+		--include-module=pikaur \
+		--include-module=pypyalpm \
+		--include-module=pyalpm \
+		--include-module=pycman \
+		--include-module=pacman_fallback \
+		\
+		./pikaur-static.py
---
+++ pikaur_static/pikaur-static.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3 -u
+"""Licensed under GPLv3, see https://www.gnu.org/licenses/"""
+
+from pikaur.main import main
+
+if __name__ == "__main__":
+    main()

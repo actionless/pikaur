2a43d629c769a00de9c318fc61ea45e1eed30d62
actionless <actionless.loveless@gmail.com>
2024-09-03 20:49:29 +0200

    fix(pikaur-static: entrypoint): set PIKAUR_NAME to pikaur-static
    chore(pikaur-static: pkgbuild): remove solved comment

--- pikaur_static/pikaur-static.py
+++ pikaur_static/pikaur-static.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3 -u
 """Licensed under GPLv3, see https://www.gnu.org/licenses/"""
 
-from pikaur.main import main
+import os
+PIKAUR_NAME_ENV_NAME = "PIKAUR_NAME"
+os.environ[PIKAUR_NAME_ENV_NAME] = os.environ.get(PIKAUR_NAME_ENV_NAME, "pikaur-static")
+from pikaur.main import main  # pylint: disable=wrong-import-position  # noqa: E402
 
 if __name__ == "__main__":
     main()

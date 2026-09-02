4f993ee0b7dba8f4a1d6b4abd552d5432f0ac651
actionless <actionless.loveless@gmail.com>
2026-09-02 20:51:35 +0200

    style(test: arch_wiki): remove unused import

--- pikaur_test/test_arch_wiki.py
+++ pikaur_test/test_arch_wiki.py
@@ -1,7 +1,11 @@
 """Licensed under GPLv3, see https://www.gnu.org/licenses/"""
 # mypy: disable-error-code=no-untyped-def
 
-from pikaur_test.helpers import PikaurDbTestCase, fake_pikaur, pikaur
+from pikaur_test.helpers import (
+    PikaurDbTestCase,
+    fake_pikaur,
+    # pikaur,
+)
 
 
 class ArchWikiTest(PikaurDbTestCase):

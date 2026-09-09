e700a56a6bff9ad7329d8829fb174f84721aa2e2
Nicolas Arsenault <arsenault.nic@gmail.com>
2026-09-09 19:20:27 -0400

    fix lint

--- pikaur/install_cli.py
+++ pikaur/install_cli.py
@@ -1050,8 +1050,8 @@ class InstallPackagesCLI:
                 elif diff_pager == DiffPagerValues.NEVER:
                     git_args = ["env", "GIT_PAGER=cat"]
                 git_cmd = PikaurConfig().review.GitCmd.get_str()
-                for s in shlex.split(git_cmd):
-                    git_args += [s.replace("{{repo_path}}", str(pkg_build.repo_path))
+                for arg in shlex.split(git_cmd):
+                    git_args += [arg.replace("{{repo_path}}", str(pkg_build.repo_path))
                                  .replace("{{last_install_hash}}", pkg_build.last_installed_hash)
                                  .replace("{{current_hash}}", pkg_build.current_hash),
                                  ]
